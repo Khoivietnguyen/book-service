af15792eb02926fb10a9032ef89c1df72dc81ab4
Khoi <nvkhoi@IfiSolution.local>
2024-05-30 09:02:07 +0700

    Update review frontend

--- book/src/main/resources/data.sql
+++ book/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 create table if not exists t_books (id bigint not null, author varchar(100) not null, nb_of_pages integer, year_pub integer, title varchar(100) not null, primary key (id));
-create sequence if not exists t_books_SEQ start with 1 increment by 1;
+-- create sequence if not exists t_books_SEQ start with 1 increment by 1;
 
 INSERT INTO t_books (id, title, author, nb_of_pages, year_pub) VALUES
 (1, 'Understanding Quarkus', 'Antonio', 200, 2015),
