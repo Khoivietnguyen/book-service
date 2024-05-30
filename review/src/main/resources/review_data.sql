7dc6bffdc29765194ecb39aead8207293703d86f
Khoi <nvkhoi@IfiSolution.local>
2024-05-30 17:30:17 +0700

    Update db source

--- review/src/main/resources/review_data.sql
+++ review/src/main/resources/review_data.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS review_db;
-CREATE DATABASE review_db;
+--DROP DATABASE IF EXISTS review_db;
+--CREATE DATABASE review_db;
 
 create table if not exists t_reviews (id bigint not null, review varchar(300) not null, score integer, book_id integer, primary key (id));
 -- create sequence if not exists t_reviews_SEQ start with 1 increment by 1;
